273def174bae2b4ca126ba518dadf1cb45e57129
tamannapatel247 <tamannapatelb500@gmail.com>
2026-01-19 21:52:33 +0530

    new modual

--- index.py
+++ index.py
@@ -1,9 +1,12 @@
 print("Your welcome In the library")
 
-username = input("enter username : ")
-password = input("enter password : ")
+# username = input("enter username : ")
+# password = input("enter password : ")
 
-if username == "librarian" and password == "lib123":
+print("enter 1 for libraian\n enter 2 for manager")
+user = int(input("enter choice:"))
+
+if user==1:
 
     print("please select,what you want to do")
 
@@ -34,6 +37,10 @@ if username == "librarian" and password == "lib123":
     elif choice == 3:
         book_name = input("Enter book name to return: ")
 
+        print("issued book:")
+        for item in issued_books:
+            print(item)
+
         if book_name in issued_books:
             issued_books.remove(book_name)
             library_books.append(book_name)
@@ -52,26 +59,39 @@ if username == "librarian" and password == "lib123":
     else:
         print("Invalid choice")
 
-elif username == "manager" and password == "man123":
-    print("\n Manager Login Successful")
+elif user==2:
+    print("\n Manager Login Successful , plz select your action")
 
     print("1. View Total Books")
-    print("2. System Summary")
+    print("2. insert book")
+    print("3. delete book")
+
+    library_books = ["1.To Kill a Mockingbird","2.Pride and Prejudice", "3.The Great Gatsby", "4.1984", "5.The Lord of the Rings", "6.The Little Prince", "7.A Tale of Two Cities", "8.Dune","9.modern hits","10.The Silent Patient"]
 
     choice = int(input("Enter your choice: "))
 
     if choice == 1:
-        total = 0
-        book_list = []
-        for library_books in books.values():
-            total = total +  len(book_list)
-        print(" Total books:", total)
+        print("\n Library Books:\n")
+        for book in library_books:
+            print(book)
 
+    if choice == 2:
+        new_book = input("Enter book name to insert: ")
+        library_books.append(new_book)
+        print("\nBook added successfully!\n")
+        print(library_books)
+
+    elif choice == 3:
+        del_book = input("Enter book name to delete: ")
+        if del_book in library_books:
+            library_books.remove(del_book)
+            print("\nBook deleted successfully!\n")
+        else:
+            print("\nBook not found!")
     else:
-        print("Invalid choice")
-
+        print("Invalid")
 else:
-    print("Invalid username or password")
+    print("Invalid")
 
 
 
